812fec012080b4930af162f9098f02aa0b32b2c4
Marcos Varela Orchillés <makarron453@gmail.com>
2026-02-20 00:05:54 +0100

    2

--- src/Controller/CircuitoController.php
+++ src/Controller/CircuitoController.php
@@ -4,15 +4,21 @@ namespace App\Controller;
 
 use PDO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CircuitoController extends AbstractController
 {
     #[Route('/', name: 'app_circuito_index')]
-    public function index(PDO $pdo): Response
+    public function index(PDO $pdo, Request $request): Response
     {
-        // Prueba rápida de PDO con Consulta Preparada (Requisito Anexo I)
+        // 1. OBLIGAMOS a la sesión a despertar para que Twig pueda leer tu usuario
+        if ($request->hasSession()) {
+            $request->getSession()->start();
+        }
+
+        // 2. Cargamos las categorías
         $stmt = $pdo->prepare("SELECT * FROM categorias ORDER BY nombre ASC");
         $stmt->execute();
         $categorias = $stmt->fetchAll();
